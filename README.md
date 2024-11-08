1ee1470827029fc13ffeea489d751958d9d54034
jaycode <seizethemoment099@gmail.com>
2024-11-08 09:54:50 +0900

    Init Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Web-Project-Team-4
+
+Front-End 레파지토리입니다.
+
+- 상세 설명 추가 예정입니다.
---
+++ src/pages/Start.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Start() {
+  return <h1>Start Project!</h1>;
+}
+
+export default Start;
