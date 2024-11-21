05d1ebdbecd11781a3d8c2307905c78cae788613
kimgs888 <kimgs8882005@naver.com>
2024-11-21 16:34:01 +0900

    review_update

--- src/API/groupMy.js
+++ src/API/groupMy.js
@@ -29,3 +29,22 @@ export const fetchMyGroups = async (
   console.log(response);
   return response.data; // 데이터 반환
 };
+
+/**
+ * 서버로 리뷰데이터를 요청하는 함수
+ * @param {Object} options 요청 옵션 (카테고리, 페이지 번호, 페이지 크기, 검색어, 지역, 모집 중 여부 등)
+ * @returns {Promise} 서버에서 반환된 데이터
+ */
+export const fetchMyReview = async ({ currentPage = 1, pageSize = 9 }) => {
+  const path =
+    process.env.REACT_APP_API_BASE_URL + `my/review/${currentPage}/${pageSize}`;
+  console.log(path);
+  const response = await axios.get(path, {
+    headers: {
+      Authorization: `${localStorage.getItem("token")}`,
+    },
+    withCredentials: true,
+  });
+  console.log(response);
+  return response.data; // 데이터 반환
+};
